99228eafe51442d631af3084acd548bafab04ef8
Angelina Lagunova <lagunova_gelya@mail.ru>
2025-10-13 15:38:16 +1000

    modified:   build/asset-manifest.json 	modified:   build/index.html 	renamed:    build/static/js/main.82d2f11a.js -> build/static/js/main.660e435e.js 	renamed:    build/static/js/main.82d2f11a.js.LICENSE.txt -> build/static/js/main.660e435e.js.LICENSE.txt 	renamed:    build/static/js/main.82d2f11a.js.map -> build/static/js/main.660e435e.js.map 	modified:   src/api.js

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 // src/api.js
 import axios from 'axios';
 
-// const API_URL = 'http://учивыучи.рф:8000/api/v1/basevocabs/';
-const API_URL = 'http://127.0.0.1:8000/api/v1/basevocabs/';
+const API_URL = 'http://учивыучи.рф:8000/api/v1/basevocabs/';
+// const API_URL = 'http://127.0.0.1:8000/api/v1/basevocabs/';
 
 
 export const fetchWords = async () => {
